4a52e2eb12e42a1954a060038338e8f7deee3400
tsunami1709 <thangnq53.hrc@gmail.com>
2019-08-11 15:19:39 +0700

    Delete test_2.py

--- test_2.py
+++
@@ -1,7 +0,0 @@
-a = ['Dac nhan', 'dac', 'dan', 'nhan']
-str = "Dac nhan tam"
-for v in a:
-    if v.lower() in str.lower():
-        print('some of the strings found in str')
-    else:
-        print('no strings found in str')
